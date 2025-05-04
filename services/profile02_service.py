95da342a96d1162bbdde1a8bcf62f710f27d02a3
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2025-05-04 13:39:55 +0900

    fix: 로깅 추가

--- services/profile02_service.py
+++ services/profile02_service.py
@@ -1,4 +1,5 @@
 import openai
+import logging
 from flask import Flask
 from config import load_secrets
 from models.quiz_models import QuizQuestion, QuizOption
@@ -18,6 +19,8 @@ client = openai.OpenAI(api_key=API_KEY)
 
 # Flask 앱 초기화
 app = Flask(__name__)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -115,9 +118,9 @@ def generate_nickname(prompt, job):
         temperature=0.7,
         max_tokens=100
     )
-    print("▶ generate_profile 호출 전 job:", repr(job))
+    logger.info("▶ generate_profile 호출 전 job:", repr(job))
     nickname1 = response.choices[0].message.content.strip() + ' ' + job
-    print("▶ nickname1:", repr(nickname1))
+    logger.info("▶ nickname1:", repr(nickname1))
 
     return nickname1
 
